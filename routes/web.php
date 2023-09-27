764154d5aed82532dc129ddd2ca11274432e189a
MayStp <maycleansitepu0204@mail.ugm.ac.id>
2023-09-27 15:38:04 +0700

    Update web.php
    pemebenaran

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,19 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 //route resource
-Route::resource('/post',PostController::class);
+Route::resource('posts',PostController::class);
 
-Route::get('/',[PostController::class,'index']); 
+// Route::get('/',[PostController::class,'index']); 
 
-Route::get('/posts/create', 'App\Http\Controllers\PostController@create')->name('posts.create');
-Route::delete('/posts/{id}', 'App\Http\Controllers\PostController@destroy')->name('posts.destroy');
-Route::get('/posts/{id}', 'App\Http\Controllers\PostController@show')->name('posts.show');
-Route::get('/posts/{id}/edit', 'App\Http\Controllers\PostController@edit')->name('posts.edit');
-Route::put('/posts/{id}', 'App\Http\Controllers\PostController@update')->name('posts.update');
-Route::post('/posts', 'App\Http\Controllers\PostController@store')->name('posts.store');
+// Route::get('/posts/create', 'App\Http\Controllers\PostController@create')->name('posts.create');
+// Route::delete('/posts/{id}', 'App\Http\Controllers\PostController@destroy')->name('posts.destroy');
+// Route::get('/posts/{id}', 'App\Http\Controllers\PostController@show')->name('posts.show');
+// Route::get('/posts/{id}/edit', 'App\Http\Controllers\PostController@edit')->name('posts.edit');
+// Route::put('/posts/{id}', 'App\Http\Controllers\PostController@update')->name('posts.update');
+// Route::post('/posts', 'App\Http\Controllers\PostController@store')->name('posts.store');
 
